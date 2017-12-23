580cc5446aa072ee018959f484d1ab28a3716b6a
dok <Dokthar@users.noreply.github.com>
2017-12-23 14:53:07 +0100

    material: add shader ref

--- src/material.h
+++ src/material.h
@@ -1,14 +1,15 @@
-#include "linear_algebra.h"
-
 #ifndef MATERIAL_H
 #define MATERIAL_H
 
-struct Material
-{
+#include "linear_algebra.h"
+#include "shader.h"
+
+struct Material {
     Vec3 ambient;
     Vec3 diffuse;
     Vec3 specular;
     float shininess;
+    GLuint shader;
 };
 
 #endif
